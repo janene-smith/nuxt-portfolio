461e17d281cc348d5e3e677661ac2341ce4b2d62
Janene Smith <j.smith.peachy.bit@gmail.com>
2022-11-04 22:19:41 -0700

    Added grid.vue page and images that belong to page

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
             <NuxtLink to="/">Home</NuxtLink> |
             <NuxtLink to="/gallery">Gallery</NuxtLink> |
             <NuxtLink to="/history">History</NuxtLink> |
+            <NuxtLink to="/history">Rememberance</NuxtLink> 
         </nav>
         <Nuxt />
         <!-- This is where you would put the  footer  -->
