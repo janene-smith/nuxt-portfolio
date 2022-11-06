fb674933362b170421d99792e0ffac78f4149c55
Janene Smith <j.smith.peachy.bit@gmail.com>
2022-11-06 15:01:31 -0800

    Saving point during troubleshooting errors.

--- pages/grid.vue
+++ pages/grid.vue
@@ -1,19 +1,18 @@
 <template>
-    <div id="app">
-        <div class="main">
+  <div id="app">
       <h1 class="ml-2 mb-1">Rememberance</h1>
-          <img class="img fluid" src="./static/AdobeStock_26591710.jpeg" width=100% height=250px  alt="The American WW2 cemetery at Ardennes by Thor Jorgen Udvang" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="The American WW2 cemetery at Ardennes by Thor Jorgen Udvang" />
+          <!-- <img class="img fluid" src="./static/AdobeStock_26591710.jpeg" width=100% height=250px  alt="The American WW2 cemetery at Ardennes by Thor Jorgen Udvang" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="The American WW2 cemetery at Ardennes by Thor Jorgen Udvang" />
             <title>The American WW2 cemetery at Ardennes by Thor Jorgen Udvang</title>
             <rect width=100% height=100% ></rect>
-            <text class="bg-dark text-white text-right mb-0 text-wrap" x=50% y=50% dy=.10em>The American WW2 cemetery at Ardennes by Thor Jorgen Udvang</text>
+            <text class="bg-dark text-white text-right mb-0 text-wrap" x=50% y=50% dy=.10em>The American WW2 cemetery at Ardennes by Thor Jorgen Udvang</text> -->
       <!-- Creates three equal-width columns on sm,md,l,xl devices using predefined grid classes. Columns centered in page with parent .container -->
-      <div class="container-fluid">
-          <div class="row">
+      
+          <div>
             <div class="col-sm-4 bg-dark pb-3">
               <h2 class="text-center mt-2">Gerald N. Croom</h2>
               <h5 class="text-center">July 20, 1921 ~ Hardy, Arkansas</h5>
               <h5 class="text-center">January 22, 2009 ~ Portland, Oregon</h5>
-              <img class="img-fluid rounded mx-auto d-block" src="./static/Croom-Portillo-Studio-Laredo-Texas-th.jpg" alt="Military Photograph of Gerald N. Croom, Laredo, Texas">
+              <!-- <img class="img-fluid rounded mx-auto d-block" src="./static/Croom-Portillo-Studio-Laredo-Texas-th.jpg" alt="Military Photograph of Gerald N. Croom, Laredo, Texas"> -->
               <p class="pt-2 font-italic text-center">Interment: Willamette National Cemetery</p>
               <p class="pl-3 pr-3">His brows were drawn into two hard black lines, while his eyes shone out from beneath them with a steely glitter. His face was bent downward, his shoulders bowed, his lips compressed, and the veins stood out like whipcord in his long, sinewy neck. His nostrils seemed to dilate with a purely animal lust for the chase, and his mind was so absolutely concentrated upon the matter before him that a question or remark fell unheeded upon his ears, or, at the most, only provoked a quick, impatient snarl in reply.</p>
             </div>
@@ -21,7 +20,7 @@
               <div class="container-fluid">
                 <div class="align-items-start">
                   <h4 class="mt-3">Gerald N. Croom Army Registration Card</h4>
-                  <img class="img-fluid rounded d-block" src="./static/G-Croom-WWII-registration-front-028-th.jpg" alt="Gerald N. Croom Army Registration Card - Front">
+                  <!-- <img class="img-fluid rounded d-block" src="./static/G-Croom-WWII-registration-front-028-th.jpg" alt="Gerald N. Croom Army Registration Card - Front"> -->
                 </div>
                 <div class="container-fluid justify-content-around">
                   <!-- Custom Blockquote -->
@@ -38,7 +37,7 @@
                 </div>
                 <div>
                   <h4 class="my-3">American Flag Signed by Military Police Army Unit</h4>
-                  <img class="img-fluid rounded d-block" src="./static/American Flag with MP unit names-Croom-WWII.jpg" alt="American Flag signed by MP soldiers during WWII">
+                  <!-- <img class="img-fluid rounded d-block" src="./static/American Flag with MP unit names-Croom-WWII.jpg" alt="American Flag signed by MP soldiers during WWII"> -->
                   <p class="italic">This U.S. flag was in my Great Uncles scrap album with signatures from fellow MP soldiers. Gerald N Croom signed top right corner.</p>
                 </div>
 
@@ -58,12 +57,12 @@
               <!-- END Blockquote -->
               </div>
           </div>
-        </div>
+      
 
         <div class="row my-3">
             <div class="col-md">
                 <a class="text-decoration-none" href="./imgs/AdobeStock_69712984.jpeg" target="_blank">
-                  <img src="./static/AdobeStock_69712984.jpeg"  class="img-thumbnail-g mb-1 img-fluid rounded mx-auto d-block" alt="Soldier statue memorial. Omaha beach, France by pio3" style="width:100%">
+                  <!-- <img src="./static/AdobeStock_69712984.jpeg"  class="img-thumbnail-g mb-1 img-fluid rounded mx-auto d-block" alt="Soldier statue memorial. Omaha beach, France by pio3" style="width:100%"> -->
                   <div class="text-center text-dark">
                     <p>Soldier statue memorial. Omaha beach, France</p>
                   </div>
@@ -71,7 +70,7 @@
             </div>
             <div class="col-md-4">
                 <a class="text-decoration-none" href="./imgs/AdobeStock_136873346.jpeg" target="_blank">
-                  <img src="./static/AdobeStock_136873346.jpeg" class="img-thumbnail-g mb-1" alt="World War II Memorial, Washington D.C. by justinfegan" style="width:100%">
+                  <!-- <img src="./static/AdobeStock_136873346.jpeg" class="img-thumbnail-g mb-1" alt="World War II Memorial, Washington D.C. by justinfegan" style="width:100%"> -->
                   <div class="text-center text-dark">
                     <p>World War II Memorial, Washington D.C.</p>
                   </div>
@@ -79,7 +78,7 @@
             </div>
             <div class="col-md-4">
                 <a class="text-decoration-none" href="./imgs/AdobeStock_199472126.jpeg" target="_blank">
-                  <img src="./static/AdobeStock_199472126.jpeg"  class="img-thumbnail-g mb-1" alt="World War II D Day memorial on Juno Beach, Graye sur mer, France by thomathzac23" style="width:100%">
+                  <!-- <img src="./static/AdobeStock_199472126.jpeg"  class="img-thumbnail-g mb-1" alt="World War II D Day memorial on Juno Beach, Graye sur mer, France by thomathzac23" style="width:100%"> -->
                   <div class="text-center text-dark">
                     <p class="mb-0">World War II D Day memorial on Juno Beach</p>
                     <p class="mt-0">Graye sur mer, France</p>
@@ -87,7 +86,7 @@
                 </a>
             </div>
           </div>
-        </div>
+       
         <div class="row" height="350">
             <div class="col-xl p-3">
               <h5 class="text-center">Navigation Links</h5>
@@ -96,17 +95,83 @@
               <h5 class="text-center">Links</h5>
             </div>
         </div>
-      </div>
-    </div>
-
-
-
-    </div>
-
+  </div>
+</div>
 </template>
 <script>
 
 </script>
-<style>
+<style lang="scss">
+/* Grid area */
+.container2 {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: 149px 81px 649px 1fr 152px;
+  grid-template-areas:
+    'header header header header header'
+    'nav nav nav nav nav'
+    'sidebar main main main main'
+    'footer footer footer footer footer';
+}
+
+/* .item1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding:  1em;
+    color: $white;
+    font-family $font-fam;
+    font-size: $fontsize;
+} */
+/* Grid container - history page */
+.container-fluid {
+  display: grid;
+}
+.col {
+  max-width: 30%;
+  /* .card {
+    width: 100%;
+  } */
+}
+.col-sm-4 {
+  background-color: bg-dark;
+  color: #ffffe6;
+}
+.col-sm-8 {
+  background-color: bg-light;
+  color: #0a0a0a;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+
+.col-xl {
+  background-color: #000000;
+  color: #ffffff;
+  padding: 5;
+}
+.blockquote-custom {
+  position: relative;
+  font-size: 1.1rem;
+  font-family: fantasy;
+  max-width:600px;
+  z-index: 100;
+}
+// thumbnails in grid/rememberance Page
+img.img-thumbnail-g {
+  float: left;
+  width: 378px;
+  height: 330px;
+  object-fit: cover;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+}
+img.img-thumbnail-g:hover {
+  box-shadow: 0 0 0 0;
+}
+.caption {
+  font-size: -50%;
+}
+
 
 </style>
