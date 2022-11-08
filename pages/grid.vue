16431b23e32c26a10e7c7ab717818294055cbb88
Janene Smith <j.smith.peachy.bit@gmail.com>
2022-11-07 16:37:22 -0800

    Fixed grid.vue static path.

--- pages/grid.vue
+++ pages/grid.vue
@@ -4,7 +4,7 @@
             
       </nuxt-link>
       <h1 class="ml-2 mb-1">Rememberance</h1>
-          <img class="img fluid" src="./static/AdobeStock_26591710.jpeg" width=100% height=250px  alt="The American WW2 cemetery at Ardennes by Thor Jorgen Udvang" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="The American WW2 cemetery at Ardennes by Thor Jorgen Udvang" />
+          <img class="img fluid" src="../static/AdobeStock_26591710.jpeg" width=100% height=250px  alt="The American WW2 cemetery at Ardennes by Thor Jorgen Udvang" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="The American WW2 cemetery at Ardennes by Thor Jorgen Udvang" />
             <title>The American WW2 cemetery at Ardennes by Thor Jorgen Udvang</title>
             <rect width=100% height=100% ></rect>
             <text class="bg-dark text-white text-right mb-0 text-wrap" x=50% y=50% dy=.10em>The American WW2 cemetery at Ardennes by Thor Jorgen Udvang</text>
@@ -15,7 +15,7 @@
               <h2 class="text-center mt-2">Gerald N. Croom</h2>
               <h5 class="text-center">July 20, 1921 ~ Hardy, Arkansas</h5>
               <h5 class="text-center">January 22, 2009 ~ Portland, Oregon</h5>
-              <!-- <img class="img-fluid rounded mx-auto d-block" src="./static/Croom-Portillo-Studio-Laredo-Texas-th.jpg" alt="Military Photograph of Gerald N. Croom, Laredo, Texas"> -->
+              <img class="img-fluid rounded mx-auto d-block" src="../static/Croom-Portillo-Studio-Laredo-Texas-th.jpg" alt="Military Photograph of Gerald N. Croom, Laredo, Texas">
               <p class="pt-2 font-italic text-center">Interment: Willamette National Cemetery</p>
               <p class="pl-3 pr-3">His brows were drawn into two hard black lines, while his eyes shone out from beneath them with a steely glitter. His face was bent downward, his shoulders bowed, his lips compressed, and the veins stood out like whipcord in his long, sinewy neck. His nostrils seemed to dilate with a purely animal lust for the chase, and his mind was so absolutely concentrated upon the matter before him that a question or remark fell unheeded upon his ears, or, at the most, only provoked a quick, impatient snarl in reply.</p>
             </div>
@@ -23,7 +23,7 @@
               <div class="container-fluid">
                 <div class="align-items-start">
                   <h4 class="mt-3">Gerald N. Croom Army Registration Card</h4>
-                  <!-- <img class="img-fluid rounded d-block" src="./static/G-Croom-WWII-registration-front-028-th.jpg" alt="Gerald N. Croom Army Registration Card - Front"> -->
+                  <img class="img-fluid rounded d-block" src="../static/G-Croom-WWII-registration-front-028-th.jpg" alt="Gerald N. Croom Army Registration Card - Front">
                 </div>
                 <div class="container-fluid justify-content-around">
                   <!-- Custom Blockquote -->
@@ -40,7 +40,7 @@
                 </div>
                 <div>
                   <h4 class="my-3">American Flag Signed by Military Police Army Unit</h4>
-                  <!-- <img class="img-fluid rounded d-block" src="./static/American Flag with MP unit names-Croom-WWII.jpg" alt="American Flag signed by MP soldiers during WWII"> -->
+                  <img class="img-fluid rounded d-block" src="../static/American Flag with MP unit names-Croom-WWII.jpg" alt="American Flag signed by MP soldiers during WWII">
                   <p class="italic">This U.S. flag was in my Great Uncles scrap album with signatures from fellow MP soldiers. Gerald N Croom signed top right corner.</p>
                 </div>
 
@@ -65,7 +65,7 @@
         <div class="row my-3">
             <div class="col-md">
                 <a class="text-decoration-none" href="./imgs/AdobeStock_69712984.jpeg" target="_blank">
-                  <!-- <img src="./static/AdobeStock_69712984.jpeg"  class="img-thumbnail-g mb-1 img-fluid rounded mx-auto d-block" alt="Soldier statue memorial. Omaha beach, France by pio3" style="width:100%"> -->
+                  <img src="../static/AdobeStock_69712984.jpeg"  class="img-thumbnail-g mb-1 img-fluid rounded mx-auto d-block" alt="Soldier statue memorial. Omaha beach, France by pio3" style="width:100%">
                   <div class="text-center text-dark">
                     <p>Soldier statue memorial. Omaha beach, France</p>
                   </div>
@@ -73,7 +73,7 @@
             </div>
             <div class="col-md-4">
                 <a class="text-decoration-none" href="./imgs/AdobeStock_136873346.jpeg" target="_blank">
-                  <!-- <img src="./static/AdobeStock_136873346.jpeg" class="img-thumbnail-g mb-1" alt="World War II Memorial, Washington D.C. by justinfegan" style="width:100%"> -->
+                  <img src="../static/AdobeStock_136873346.jpeg" class="img-thumbnail-g mb-1" alt="World War II Memorial, Washington D.C. by justinfegan" style="width:100%">
                   <div class="text-center text-dark">
                     <p>World War II Memorial, Washington D.C.</p>
                   </div>
@@ -81,7 +81,7 @@
             </div>
             <div class="col-md-4">
                 <a class="text-decoration-none" href="./imgs/AdobeStock_199472126.jpeg" target="_blank">
-                  <!-- <img src="./static/AdobeStock_199472126.jpeg"  class="img-thumbnail-g mb-1" alt="World War II D Day memorial on Juno Beach, Graye sur mer, France by thomathzac23" style="width:100%"> -->
+                  <img src="../static/AdobeStock_199472126.jpeg"  class="img-thumbnail-g mb-1" alt="World War II D Day memorial on Juno Beach, Graye sur mer, France by thomathzac23" style="width:100%">
                   <div class="text-center text-dark">
                     <p class="mb-0">World War II D Day memorial on Juno Beach</p>
                     <p class="mt-0">Graye sur mer, France</p>
@@ -117,15 +117,6 @@
     'footer footer footer footer footer';
 }
 
-/* .item1 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding:  1em;
-    color: $white;
-    font-family $font-fam;
-    font-size: $fontsize;
-} */
 /* Grid container - history page */
 .container-fluid {
   display: grid;
